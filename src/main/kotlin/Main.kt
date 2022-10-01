9d0ceb979574b0c216244db9fa9927ad73e21654
Your Name <you@example.com>
2022-10-02 01:17:57 +0200

    Update Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,178 @@ fun  main(){
 //    println("waled saied")                  ==>  Print OutPut with line
 //    print("Hello ")                         ==>  Print OutPut inline
 
-
+// ________________________________________________________________________________________________________________
 
 //            // ==> Comment to refer info about thing
 
+// ________________________________________________________________________________________________________________
+
+//
+//    var  firstName =  "waled "
+//    var  lastName =  "Saied"
+//    var age = 21
+//
+//    print(firstName )
+//    println(lastName)
+//    println(age)
+
+// ________________________________________________________________________________________________________________
+
+// difference between val & var  ==> var can access value , read and update it but val access value and read it not updated data
+
+//    val firstName = "Ali"
+//      //    firstName = "waled" ==> Error Not Updated data
+//   print(firstName)
+//
+
+
+// ________________________________________________________________________________________________________________
+
+//    var car:String = "Volks"
+//    println(car)       //  ==> provide type on Kotlin Variable
+
+//    var numeric:Int = 123
+//    println(numeric)
+
+// ________________________________________________________________________________________________________________
+//
+//    // number DataType(right or decimal)
+//    val  myByte:Byte = 8;
+//    val  myShort:Short = 16;
+//    val  myInt:Byte = 32;
+//    val  myLong:Byte = 64;
+//
+//    val  myFloat:Float = 32.00F;
+//    val  myDouble:Double = 64.00;
+//
+//    println(myShort.toShort())
+
+// ________________________________________________________________________________________________________________
+
+
+//    var data:String  =  "Hello World"
+//    println(data.uppercase())
+//    println(data.lowercase())
+//
+//
+//    var charA:Char = 'A'
+//    println(charA)
+//
+
+
+// ________________________________________________________________________________________________________________
+
+
+//    // Deployment Variable
+//
+//    var name:String = "Waled Saied "
+//    var age:Int = 21
+//
+//    println("My Name is $name and i'm $age years old")
+//
+//
+
+
+// ________________________________________________________________________________________________________________
+
+//
+//    var dFalse:Boolean = false
+//    println(dFalse)
+//    println(dFalse.not())
+// ________________________________________________________________________________________________________________
+
+
+//    val num1:Double = 10.5
+//    val num2:Double = 3.5
+//    var result:Double = num1+num2
+//    println(result)
+
+    // Arithmetic Operation ==> + - * / %
+
+// ________________________________________________________________________________________________________________
+
+//
+//    var fName:String = "Waled "
+//    var lName:String = "Saied "
+//    var jop:String = "Software Engineer "
+//
+//    var res = fName + " "+ lName + " "+ jop
+//    println(res)
+//
+//    println("My Name is $fName $lName and my jop is ")
+
+// ________________________________________________________________________________________________________________
+
+//    var number:Int = 5;
+//    number += 10
+//    println(number) ==> result = 15 (Assignment operator += -= *= /=)
+// ________________________________________________________________________________________________________________
+
+
+    // ++ increment
+    // -- decrement
+    // ++a ==> prefix === a++ postfix
+
+//    var a:Int = 5
+//    println(a++)
+//    println(++a)
+
+// ________________________________________________________________________________________________________________
+    // comparison operator (> < == !=)
+
+//    var x:Int = 10
+//    if(x < 10){
+//        println("No")
+//    }else {
+//        println("yes")
+//    }
+
+// ________________________________________________________________________________________________________________
+// if - else if - else
+//    var price:Int = 5000
+//    if(price == 5000){
+//        println("Done")
+//    }else if (price < 200){
+//        println("Loss")
+//    }else{
+//        println("winner")
+//    }
+// ________________________________________________________________________________________________________________
+
+//    var myAge = 25
+//    if(myAge == 25) print("my Age is $myAge")else{print("my Age is Not $myAge")}
+//
+//    // Single line condition
+// ________________________________________________________________________________________________________________
+
+    // When Expression
+//
+//    class  User(var fName:String ,var lName:String ,)
+//
+//    val user = User("Waled", "Saied")
+//
+//    when(user.fName){
+//        "Waled" -> println("This is First Name")
+//        "Saied" -> println("This is Second Name")
+//        else ->println("This is Not Right Person")
+//    }
+
+
+
+// ________________________________________________________________________________________________________________
+// For Loop
+//    val values = listOf(1,2,3,4,5,6,7,8)
+//    for( i in values){
+//        println(i)
+//    }
+//
+//
+//    for(i in 0 until 10){
+//        print(i)
+//    }            // ==> print form 0 to 9
+
+// ________________________________________________________________________________________________________________
 
 
 }
+
